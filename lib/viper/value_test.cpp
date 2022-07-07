bf941aaac169100b8afc27b88bbb1fa6e686fc6d
Uditha Atukorala <ua@nuked.zone>
2022-07-07 08:15:32 +0100

    (viper) cleanup tests

--- lib/viper/value_test.cpp
+++ lib/viper/value_test.cpp
@@ -12,41 +12,43 @@ TEST(viper, value_constructor) {
 }
 
 TEST(viper, value_conversions) {
-	const char data[] = "0....5...9";
-
-	// Full length
+	// std::string_view
 	{
-		std::string_view v = viper::value(data);
-		EXPECT_EQ("0....5...9"sv, v);
-	}
+		// From ryml::NodeRef
+		{
+			auto             t = ryml::parse_in_arena(R"(key: value)");
+			std::string_view v = viper::value(t["key"]);
+			EXPECT_EQ("value"sv, v);
+		}
 
-	// Partials
-	{
-		std::string_view v = viper::value(data, 6);
-		EXPECT_EQ("0....5"sv, v);
+		// From null ryml::NodeRef
+		{
+			std::string_view v = viper::value(ryml::NodeRef{});
+			EXPECT_EQ(0, v.size());
+			EXPECT_EQ(""sv, v);
+		}
 
-		v = viper::value(data + 5, 5);
-		EXPECT_EQ("5...9"sv, v);
-	}
+		const char data[] = "0....5...9";
+		// Full length
+		{
+			std::string_view v = viper::value(data);
+			EXPECT_EQ("0....5...9"sv, v);
+		}
 
-	// Nulls
-	{
-		std::string_view v = viper::value{};
-		EXPECT_EQ(0, v.size());
-		EXPECT_EQ(""sv, v);
-	}
+		// Partials
+		{
+			std::string_view v = viper::value(data, 6);
+			EXPECT_EQ("0....5"sv, v);
 
-	// From ryml::NodeRef
-	{
-		auto             t = ryml::parse_in_arena(R"(key: value)");
-		std::string_view v = viper::value(t["key"]);
-		EXPECT_EQ("value"sv, v);
-	}
+			v = viper::value(data + 5, 5);
+			EXPECT_EQ("5...9"sv, v);
+		}
 
-	// From null ryml::NodeRef
-	{
-		std::string_view v = viper::value(ryml::NodeRef{});
-		EXPECT_EQ(0, v.size());
-		EXPECT_EQ(""sv, v);
+		// Nulls
+		{
+			std::string_view v = viper::value{};
+			EXPECT_EQ(0, v.size());
+			EXPECT_EQ(""sv, v);
+		}
 	}
 }
